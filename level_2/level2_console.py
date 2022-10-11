f25f735b2883041e0cb934485b97aadac93ac0a9
annasophiacarteromoe <annasophiacartero@gmail.com>
2022-10-11 10:16:23 +0200

    update

--- level_2/level2_console.py
+++ level_2/level2_console.py
@@ -1,11 +1,13 @@
 import pygame    
     
 pygame.init()    
-screen = pygame.display.set_mode((800,500))    
+screen = pygame.display.set_mode((1100,600))    
 done = False    
     
-while not done:    
+while not done:  
     for event in pygame.event.get():    
         if event.type == pygame.QUIT:    
-            done = True    
-    pygame.display.flip()    
+            done = True
+    pygame.display.flip()   
+
+
