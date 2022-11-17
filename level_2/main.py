d6c8df7588763e05846cf6208e061691fa2a250e
annasophiacarteromoe <annasophiacartero@gmail.com>
2022-11-17 11:13:08 +0100

    level2_working

--- level_2/main.py
+++ level_2/main.py
@@ -7,32 +7,31 @@ pygame.init()
 SCREEN_HEIGHT = 600
 SCREEN_WIDTH = 1100
 SCREEN = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+SCREEN.fill((29, 17, 53))
 
-RUNNING = [pygame.image.load(os.path.join(r"C:\Users\Anna-Sophia\Desktop\6_Computer_Games_Technology\MarsMission_Game\AU_CGT_Game\level_2\Assets\Dog\DogRun1.png")), 
-           pygame.image.load(os.path.join(r"C:\Users\Anna-Sophia\Desktop\6_Computer_Games_Technology\MarsMission_Game\AU_CGT_Game\level_2\Assets\Dog\DogRun2.png")),
-           pygame.image.load(os.path.join(r"C:\Users\Anna-Sophia\Desktop\6_Computer_Games_Technology\MarsMission_Game\AU_CGT_Game\level_2\Assets\Dog\DogRun3.png")),
-           pygame.image.load(os.path.join(r"C:\Users\Anna-Sophia\Desktop\6_Computer_Games_Technology\MarsMission_Game\AU_CGT_Game\level_2\Assets\Dog\DogRun4.png")),
-           pygame.image.load(os.path.join(r"C:\Users\Anna-Sophia\Desktop\6_Computer_Games_Technology\MarsMission_Game\AU_CGT_Game\level_2\Assets\Dog\DogRun5.png"))]
-JUMPING = pygame.image.load(os.path.join(r"C:\Users\Anna-Sophia\Desktop\6_Computer_Games_Technology\MarsMission_Game\AU_CGT_Game\level_2\Assets\Dog\dogRun3.png"))
-DUCKING = [pygame.image.load(os.path.join(r"C:\Users\Anna-Sophia\Desktop\6_Computer_Games_Technology\MarsMission_Game\AU_CGT_Game\level_2\Assets\Dog\DogRun2.png")),
-           pygame.image.load(os.path.join(r"C:\Users\Anna-Sophia\Desktop\6_Computer_Games_Technology\MarsMission_Game\AU_CGT_Game\level_2\Assets\Dog\DogRun5.png"))]
+FLYING = [pygame.image.load(os.path.join(r"C:\Users\Anna-Sophia\Desktop\6_Computer_Games_Technology\MarsMission_Game\AU_CGT_Game\level_2\Assets\Rocket\Rocket1.png")), 
+           pygame.image.load(os.path.join(r"C:\Users\Anna-Sophia\Desktop\6_Computer_Games_Technology\MarsMission_Game\AU_CGT_Game\level_2\Assets\Rocket\Rocket2.png")),
+           pygame.image.load(os.path.join(r"C:\Users\Anna-Sophia\Desktop\6_Computer_Games_Technology\MarsMission_Game\AU_CGT_Game\level_2\Assets\Rocket\Rocket3.png")),
+           pygame.image.load(os.path.join(r"C:\Users\Anna-Sophia\Desktop\6_Computer_Games_Technology\MarsMission_Game\AU_CGT_Game\level_2\Assets\Rocket\Rocket4.png"))]
+JUMPING = pygame.image.load(os.path.join(r"C:\Users\Anna-Sophia\Desktop\6_Computer_Games_Technology\MarsMission_Game\AU_CGT_Game\level_2\Assets\Rocket\Rocket4.png"))
+DUCKING = [pygame.image.load(os.path.join(r"C:\Users\Anna-Sophia\Desktop\6_Computer_Games_Technology\MarsMission_Game\AU_CGT_Game\level_2\Assets\Rocket\Rocket1.png")),
+           pygame.image.load(os.path.join(r"C:\Users\Anna-Sophia\Desktop\6_Computer_Games_Technology\MarsMission_Game\AU_CGT_Game\level_2\Assets\Rocket\Rocket2.png"))]
 
-SMALL_CACTUS = [pygame.image.load(os.path.join(r"C:\Users\Anna-Sophia\Desktop\6_Computer_Games_Technology\MarsMission_Game\AU_CGT_Game\level_2\Assets\Alien\SmallAlien1.png")),
-                pygame.image.load(os.path.join(r"C:\Users\Anna-Sophia\Desktop\6_Computer_Games_Technology\MarsMission_Game\AU_CGT_Game\level_2\Assets\Alien\SmallAlien2.png")),
-                pygame.image.load(os.path.join(r"C:\Users\Anna-Sophia\Desktop\6_Computer_Games_Technology\MarsMission_Game\AU_CGT_Game\level_2\Assets\Alien\SmallAlien3.png"))]
-LARGE_CACTUS = [pygame.image.load(os.path.join(r"C:\Users\Anna-Sophia\Desktop\6_Computer_Games_Technology\MarsMission_Game\AU_CGT_Game\level_2\Assets\Fire\Fire1.png" )),
-                pygame.image.load(os.path.join(r"C:\Users\Anna-Sophia\Desktop\6_Computer_Games_Technology\MarsMission_Game\AU_CGT_Game\level_2\Assets\Fire\Fire1.png")),
-                pygame.image.load(os.path.join(r"C:\Users\Anna-Sophia\Desktop\6_Computer_Games_Technology\MarsMission_Game\AU_CGT_Game\level_2\Assets\Fire\Fire1.png"))]
+SMALL_ALIEN = [pygame.image.load(os.path.join(r"C:\Users\Anna-Sophia\Desktop\6_Computer_Games_Technology\MarsMission_Game\AU_CGT_Game\level_2\Assets\Alien\asteroid1.png")),
+                pygame.image.load(os.path.join(r"C:\Users\Anna-Sophia\Desktop\6_Computer_Games_Technology\MarsMission_Game\AU_CGT_Game\level_2\Assets\Alien\asteroid1.png")),
+                pygame.image.load(os.path.join(r"C:\Users\Anna-Sophia\Desktop\6_Computer_Games_Technology\MarsMission_Game\AU_CGT_Game\level_2\Assets\Alien\asteroid1.png"))]
+MILKYWAY = [pygame.image.load(os.path.join(r"C:\Users\Anna-Sophia\Desktop\6_Computer_Games_Technology\MarsMission_Game\AU_CGT_Game\level_2\Assets\Alien\milkyway.png" )),
+                pygame.image.load(os.path.join(r"C:\Users\Anna-Sophia\Desktop\6_Computer_Games_Technology\MarsMission_Game\AU_CGT_Game\level_2\Assets\Alien\milkyway2.png"))]
 
 UFO = [pygame.image.load(os.path.join(r"C:\Users\Anna-Sophia\Desktop\6_Computer_Games_Technology\MarsMission_Game\AU_CGT_Game\level_2\Assets\Spaceship\Spaceship1.png")),
         pygame.image.load(os.path.join(r"C:\Users\Anna-Sophia\Desktop\6_Computer_Games_Technology\MarsMission_Game\AU_CGT_Game\level_2\Assets\Spaceship\Spaceship2.png"))]
 
-CLOUD = pygame.image.load(os.path.join(r"C:\Users\Anna-Sophia\Desktop\6_Computer_Games_Technology\MarsMission_Game\AU_CGT_Game\level_2\Assets\Other\Cloud.png"))
+PLANET = pygame.image.load(os.path.join(r"C:\Users\Anna-Sophia\Desktop\6_Computer_Games_Technology\MarsMission_Game\AU_CGT_Game\level_2\Assets\Other\Planet.png"))
 
-BG = pygame.image.load(os.path.join(r"C:\Users\Anna-Sophia\Desktop\6_Computer_Games_Technology\MarsMission_Game\AU_CGT_Game\level_2\Assets\Other\Track.png"))
+BG = SCREEN.fill((29, 17, 53))
 
 
-class Dog:
+class Rocket:
     X_POS = 80
     Y_POS = 310
     Y_POS_DUCK = 340
@@ -40,76 +39,76 @@ class Dog:
 
     def __init__(self):
         self.duck_img = DUCKING
-        self.run_img = RUNNING
+        self.run_img = FLYING
         self.jump_img = JUMPING
 
-        self.dog_duck = False
-        self.dog_run = True
-        self.dog_jump = False
+        self.rocket_duck = False
+        self.rocket_run = True
+        self.rocket_jump = False
 
         self.step_index = 0
         self.jump_vel = self.JUMP_VEL
         self.image = self.run_img[0]
-        self.dog_rect = self.image.get_rect()
-        self.dog_rect.x = self.X_POS
-        self.dog_rect.y = self.Y_POS
+        self.rocket_rect = self.image.get_rect()
+        self.rocket_rect.x = self.X_POS
+        self.rocket_rect.y = self.Y_POS
 
     def update(self, userInput):
-        if self.dog_duck:
+        if self.rocket_duck:
             self.duck()
-        if self.dog_run:
+        if self.rocket_run:
             self.run()
-        if self.dog_jump:
+        if self.rocket_jump:
             self.jump()
 
         if self.step_index >= 10:
             self.step_index = 0
 
-        if userInput[pygame.K_UP] and not self.dog_jump:
-            self.dog_duck = False
-            self.dog_run = False
-            self.dog_jump = True
-        elif userInput[pygame.K_DOWN] and not self.dog_jump:
-            self.dog_duck = True
-            self.dog_run = False
-            self.dog_jump = False
-        elif not (self.dog_jump or userInput[pygame.K_DOWN]):
-            self.dog_duck = False
-            self.dog_run = True
-            self.dog_jump = False
+        if userInput[pygame.K_UP] and not self.rocket_jump:
+            self.rocket_duck = False
+            self.rocket_run = False
+            self.rocket_jump = True
+        elif userInput[pygame.K_DOWN] and not self.rocket_jump:
+            self.rocket_duck = True
+            self.rocket_run = False
+            self.rocket_jump = False
+        elif not (self.rocket_jump or userInput[pygame.K_DOWN]):
+            self.rocket_duck = False
+            self.rocket_run = True
+            self.rocket_jump = False
 
     def duck(self):
         self.image = self.duck_img[self.step_index // 5]
-        self.dog_rect = self.image.get_rect()
-        self.dog_rect.x = self.X_POS
-        self.dog_rect.y = self.Y_POS_DUCK
+        self.rocket_rect = self.image.get_rect()
+        self.rocket_rect.x = self.X_POS
+        self.rocket_rect.y = self.Y_POS_DUCK
         self.step_index += 1
 
     def run(self):
         self.image = self.run_img[self.step_index // 5]
-        self.dog_rect = self.image.get_rect()
-        self.dog_rect.x = self.X_POS
-        self.dog_rect.y = self.Y_POS
+        self.rocket_rect = self.image.get_rect()
+        self.rocket_rect.x = self.X_POS
+        self.rocket_rect.y = self.Y_POS
         self.step_index += 1
 
     def jump(self):
         self.image = self.jump_img
-        if self.dog_jump:
-            self.dog_rect.y -= self.jump_vel * 4
+        if self.rocket_jump:
+            self.rocket_rect.y -= self.jump_vel * 4
             self.jump_vel -= 0.8
         if self.jump_vel < - self.JUMP_VEL:
-            self.dog_jump = False
+            self.rocket_jump = False
             self.jump_vel = self.JUMP_VEL
 
     def draw(self, SCREEN):
-        SCREEN.blit(self.image, (self.dog_rect.x, self.dog_rect.y))
+        SCREEN.blit(self.image, (self.rocket_rect.x, self.rocket_rect.y))
 
 
-class Cloud:
+class Planet:
     def __init__(self):
         self.x = SCREEN_WIDTH + random.randint(800, 1000)
         self.y = random.randint(50, 100)
-        self.image = CLOUD
+        self.image = PLANET
         self.width = self.image.get_width()
 
     def update(self):
@@ -138,21 +137,21 @@ class Obstacle:
         SCREEN.blit(self.image[self.type], self.rect)
 
 
-class SmallCactus(Obstacle):
+class SmallAlien(Obstacle):
     def __init__(self, image):
         self.type = random.randint(0, 2)
         super().__init__(image, self.type)
         self.rect.y = 325
 
 
-class LargeCactus(Obstacle):
+class Milkway(Obstacle):
     def __init__(self, image):
-        self.type = random.randint(0, 2)
+        self.type = random.randint(0, 1)
         super().__init__(image, self.type)
         self.rect.y = 300
 
 
-class UFO(Obstacle):
+class Ufo(Obstacle):
     def __init__(self, image):
         self.type = 0
         super().__init__(image, self.type)
@@ -170,8 +169,8 @@ def main():
     global game_speed, x_pos_bg, y_pos_bg, points, obstacles
     run = True
     clock = pygame.time.Clock()
-    player = Dog()
-    cloud = Cloud()
+    player = Rocket()
+    planet = Planet()
     game_speed = 20
     x_pos_bg = 0
     y_pos_bg = 380
@@ -186,27 +185,17 @@ def main():
         if points % 100 == 0:
             game_speed += 1
 
-        text = font.render("Points: " + str(points), True, (0, 0, 0))
+        text = font.render("Kilometers travelled: " + str(points), True, (255, 255, 255))
         textRect = text.get_rect()
-        textRect.center = (1000, 40)
+        textRect.center = (900, 40)
         SCREEN.blit(text, textRect)
-
-    def background():
-        global x_pos_bg, y_pos_bg
-        image_width = BG.get_width()
-        SCREEN.blit(BG, (x_pos_bg, y_pos_bg))
-        SCREEN.blit(BG, (image_width + x_pos_bg, y_pos_bg))
-        if x_pos_bg <= -image_width:
-            SCREEN.blit(BG, (image_width + x_pos_bg, y_pos_bg))
-            x_pos_bg = 0
-        x_pos_bg -= game_speed
 
     while run:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
 
-        SCREEN.fill((255, 255, 255))
+        SCREEN.fill((29, 17, 53))
         userInput = pygame.key.get_pressed()
 
         player.draw(SCREEN)
@@ -214,24 +203,22 @@ def main():
 
         if len(obstacles) == 0:
             if random.randint(0, 2) == 0:
-                obstacles.append(SmallCactus(SMALL_CACTUS))
+                obstacles.append(SmallAlien(SMALL_ALIEN))
             elif random.randint(0, 2) == 1:
-                obstacles.append(LargeCactus(LARGE_CACTUS))
+                obstacles.append(Milkway(MILKYWAY))
             elif random.randint(0, 2) == 2:
-                obstacles.append(UFO(UFO))
+                obstacles.append(Ufo(UFO))
 
         for obstacle in obstacles:
             obstacle.draw(SCREEN)
             obstacle.update()
-            if player.dog_rect.colliderect(obstacle.rect):
+            if player.rocket_rect.colliderect(obstacle.rect):
                 pygame.time.delay(2000)
                 death_count += 1
                 menu(death_count)
 
-        background()
-
-        cloud.draw(SCREEN)
-        cloud.update()
+        planet.draw(SCREEN)
+        planet.update()
 
         score()
 
@@ -243,21 +230,21 @@ def menu(death_count):
     global points
     run = True
     while run:
-        SCREEN.fill((255, 255, 255))
+        SCREEN.fill((29, 17, 53))
         font = pygame.font.Font('freesansbold.ttf', 30)
 
         if death_count == 0:
-            text = font.render("Press any Key to Start", True, (0, 0, 0))
+            text = font.render("Press any Key to Start", True, (255, 255, 255))
         elif death_count > 0:
-            text = font.render("Press any Key to Restart", True, (0, 0, 0))
-            score = font.render("Your Score: " + str(points), True, (0, 0, 0))
+            text = font.render("Press any Key to Restart", True, (255, 255, 255))
+            score = font.render("Kilometers travelled: " + str(points), True, (255, 255, 255))
             scoreRect = score.get_rect()
             scoreRect.center = (SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2 + 50)
             SCREEN.blit(score, scoreRect)
         textRect = text.get_rect()
         textRect.center = (SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2)
         SCREEN.blit(text, textRect)
-        SCREEN.blit(RUNNING[0], (SCREEN_WIDTH // 2 - 20, SCREEN_HEIGHT // 2 - 140))
+        SCREEN.blit(FLYING[0], (SCREEN_WIDTH // 2 - 20, SCREEN_HEIGHT // 2 - 140))
         pygame.display.update()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
