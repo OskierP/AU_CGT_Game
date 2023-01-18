6d01c6fb9cfa125065d63ad1d5c6dc95808ce64d
ecdison <106946302+ecdison@users.noreply.github.com>
2023-01-18 22:43:57 +0100

    Ellion level 2 code changes 1

--- LEVEL_2/level_2.py
+++ LEVEL_2/level_2.py
@@ -1,49 +1,65 @@
+
 import os
+import pygame
+from pygame import mixer
 import random
 
-import pygame
-
-import flags
 
 pygame.init()
+
+clock = pygame.time.Clock()
+fps = 50
 
 # Global Constants
 SCREEN_HEIGHT = 600
 SCREEN_WIDTH = 1100
 SCREEN = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-SCREEN.fill((29, 17, 53))
+DARK_BLUE = (29, 17, 53)
 
-FLYING = [pygame.image.load(os.path.join(r"LEVEL_2/Assets/Rocket/Rocket1.png")),
-          pygame.image.load(os.path.join(r"LEVEL_2/Assets/Rocket/Rocket2.png")),
-          pygame.image.load(os.path.join(r"LEVEL_2/Assets/Rocket/Rocket3.png")),
-          pygame.image.load(os.path.join(r"LEVEL_2/Assets/Rocket/Rocket4.png"))]
-JUMPING = pygame.image.load(os.path.join(r"LEVEL_2/Assets/Rocket/Rocket4.png"))
-DUCKING = [pygame.image.load(os.path.join(r"LEVEL_2/Assets/Rocket/Rocket1.png")),
-           pygame.image.load(os.path.join(r"LEVEL_2/Assets/Rocket/Rocket2.png"))]
+#load images
+rocket_img1 = pygame.image.load('Assets/Rocket/Rocket1.png')
+rocket_img2 = pygame.image.load('Assets/Rocket/Rocket2.png')
+rocket_img3 = pygame.image.load('Assets/Rocket/Rocket3.png')
+rocket_img4 = pygame.image.load('Assets/Rocket/Rocket4.png')
+asteroid_img = pygame.image.load('Assets/Alien/asteroid1.png')
+milkyway_img1 = pygame.image.load('Assets/Alien/milkyway1.png')
+milkyway_img2 = pygame.image.load('Assets/Alien/milkyway2.png')
+spaceship_img1 = pygame.image.load('Assets/Spaceship/Spaceship1.png')
+spaceship_img2 = pygame.image.load('Assets/Spaceship/Spaceship2.png')
+planet_img = pygame.image.load('Assets/Other/Planet.png')
 
-SMALL_ALIEN = [pygame.image.load(os.path.join(r"LEVEL_2/Assets/Alien/asteroid1.png")),
-               pygame.image.load(os.path.join(r"LEVEL_2/Assets/Alien/asteroid1.png")),
-               pygame.image.load(os.path.join(r"LEVEL_2/Assets/Alien/asteroid1.png"))]
-MILKYWAY = [pygame.image.load(os.path.join(r"LEVEL_2/Assets/Alien/milkyway.png")),
-            pygame.image.load(os.path.join(r"LEVEL_2/Assets/Alien/milkyway2.png"))]
+#background image
+bg_img = pygame.image.load('Assets/Other/spacebg.png').convert_alpha()
+bg_img = pygame.transform.scale(bg_img, (SCREEN_WIDTH, SCREEN_HEIGHT))
 
-UFO = [pygame.image.load(os.path.join(r"LEVEL_2/Assets/Spaceship/Spaceship1.png")),
-       pygame.image.load(os.path.join(r"LEVEL_2/Assets/Spaceship/Spaceship2.png"))]
+#load music and sounds
+# source https://www.youtube.com/watch?v=9NcPvmk4vfo
+pygame.mixer.music.load('Assets/Other/space_ride_music.mp3')
+pygame.mixer.music.set_volume(.3)
+pygame.mixer.music.play(-1, 0.0, 5000)
+FLYING = [rocket_img1, rocket_img2, rocket_img3, rocket_img4]
+JUMPING = rocket_img4
+DUCKING = [rocket_img1, rocket_img2]
+SMALL_ALIEN = [asteroid_img, asteroid_img, asteroid_img]
+MILKYWAY = [milkyway_img1, milkyway_img2]
+UFO = [spaceship_img1, spaceship_img2]
+PLANET = planet_img
 
-PLANET = pygame.image.load(os.path.join(r"LEVEL_2/Assets/Other/Planet.png"))
-
-BG = SCREEN.fill((29, 17, 53))
+def draw_bg():
+    SCREEN.fill(DARK_BLUE)
+    width = bg_img.get_width()
+    for x in range(10):
+        SCREEN.blit(bg_img, ((x * width), 0))
 
 # The base classes of this code come from the following source. Alterations were made for the purpose of Martian Mission.
 # codewmax (2020) chrome-dinosaur main.py (Version unknown) [Source code]. https://github.com/codewmax/chrome-dinosaur
 
 class Rocket:
+    Y_POS = SCREEN_HEIGHT / 2
     X_POS = 80
-    Y_POS = 310
     #Y_POS = 310 # this is y-position of the rocket
-    Y_POS_DUCK = 350 # this is the y-position of the rocket when ducking
-    JUMP_VEL = 8.5
-    DUCK_VEL = 7.0
+    JUMP_VEL = 5.0
+    DUCK_VEL = 5.0
 
     def __init__(self):
         self.duck_img = DUCKING
@@ -64,6 +80,8 @@ class Rocket:
         #self.rocket_rect.y = self.Y_POS
 
     def update(self, userInput):
+        dy = self.rocket_rect.y
+
         if self.rocket_duck:
             self.duck()
         if self.rocket_run:
@@ -74,7 +92,7 @@ class Rocket:
         if self.step_index >= 10:
             self.step_index = 0
 
-        if userInput[pygame.K_UP] and not self.rocket_jump and self.rocket_jump < SCREEN_HEIGHT:
+        if (userInput[pygame.K_UP] and not self.rocket_jump) and self.rocket_jump < SCREEN_HEIGHT:
             self.rocket_duck = False
             self.rocket_run = False
             self.rocket_jump = True
@@ -86,8 +104,18 @@ class Rocket:
             self.rocket_duck = False
             self.rocket_run = True
             self.rocket_jump = False
+        # if userInput[pygame.K_UP] and not self.rocket_jump and self.rocket_jump < SCREEN_HEIGHT:
+        #     self.rocket_rect.y -= 20
+        # elif userInput[pygame.K_DOWN] and not self.rocket_jump:
+        #     self.rocket_rect.y += 20
 
-    # Duck currently goes up then down
+        if self.rocket_rect.bottom >= SCREEN_HEIGHT:
+            self.rocket_rect.y = dy
+        if self.rocket_rect.top <= 0:
+            self.rocket_rect.y = 0
+
+
+    #Duck currently goes up then down
     def duck(self):
         self.image = self.duck_img[self.step_index // 5]
         if self.rocket_duck:
@@ -96,11 +124,6 @@ class Rocket:
         if self.duck_vel < - self.DUCK_VEL:
             self.rocket_duck = False
             self.duck_vel = self.DUCK_VEL
-        #self.image = self.duck_img[self.step_index // 5]
-        #self.rocket_rect = self.image.get_rect()
-        #self.rocket_rect.x = self.X_POS
-        #self.rocket_rect.y = self.Y_POS_DUCK
-        #self.step_index += 1
 
     def run(self):
         self.image = self.run_img[self.step_index // 5]
@@ -111,14 +134,6 @@ class Rocket:
             self.rocket_jump = False
             self.jump_vel = self.JUMP_VEL
         self.step_index += 1
-
-        #self.image = self.run_img[self.step_index // 5]
-        #self.rocket_rect = self.image.get_rect()
-        #self.rocket_rect.x = self.X_POS
-        #self.rocket_rect.y = self.Y_POS
-        #self.step_index += 1
-        #self.rocket_rect.y = self.jump_vel
-        #self.rocket_rect.y = self.rocket_rect.move_ip(0,1)
 
 
     def jump(self):
@@ -132,23 +147,6 @@ class Rocket:
 
     def draw(self, SCREEN):
         SCREEN.blit(self.image, (self.rocket_rect.x, self.rocket_rect.y))
-
-
-#class Planet:
-    #def __init__(self):
-        #self.x = SCREEN_WIDTH + random.randint(800, 1000)
-        #self.y = random.randint(50, 100)
-        #self.image = PLANET
-        #self.width = self.image.get_width()
-
-    #def update(self):
-        #self.x -= game_speed
-        #if self.x < -self.width:
-            #self.x = SCREEN_WIDTH + random.randint(2500, 3000)
-            #self.y = random.randint(50, 100)
-
-    #def draw(self, SCREEN):
-        #SCREEN.blit(self.image, (self.x, self.y))
 
 
 class Obstacle:
@@ -194,23 +192,22 @@ class Ufo(Obstacle):
         SCREEN.blit(self.image[self.index // 5], self.rect)
         self.index += 1
 
-
 def main():
-    global game_speed, x_pos_bg, y_pos_bg, points, obstacles
+    global game_speed , x_pos_bg , y_pos_bg , points , obstacles
     run = True
-    clock = pygame.time.Clock()
-    player = Rocket()
-    #planet = Planet()
-    game_speed = 20
+    clock = pygame.time.Clock ()
+    player = Rocket ()
+    # planet = Planet()
+    game_speed = 15
     x_pos_bg = 0
     y_pos_bg = 380
     points = 0
-    font = pygame.font.Font('freesansbold.ttf', 20)
+    font = pygame.font.Font('freesansbold.ttf' , 20)
     obstacles = []
     death_count = 0
 
     def score():
-        global points, game_speed
+        global points , game_speed
         points += 1
         if points % 100 == 0:
             game_speed += 1
@@ -224,15 +221,16 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
-                flags.next_lvl_2.set_flag(True)
+            # elif event.key == pygame.K_ESC:
+            #     run = False
 
         if points > 2500:  #### TO DO: Around 2000 points Mars could move to sight and then level would end. Cutscene and new level
             run = False
-            flags.next_lvl_2.set_flag(True)
 
         SCREEN.fill((29, 17, 53))
         userInput = pygame.key.get_pressed()
 
+        draw_bg()
         player.draw(SCREEN)
         player.update(userInput)
 
@@ -264,15 +262,8 @@ def main():
 def menu(death_count):
     global points
     run = True
-
     while run:
-
-        if flags.next_lvl_2.get_flag():
-            run = False
-            # pygame.display.quit()
-            # pygame.quit()
-
-        SCREEN.fill((29, 17, 53))
+        SCREEN.fill((DARK_BLUE))
         font = pygame.font.Font('freesansbold.ttf', 30)
 
         #if points == 100:
@@ -300,7 +291,7 @@ def menu(death_count):
                 main()
 
 
-# menu(death_count=0)
+menu(death_count=0)
 
 
 # It jumps whilst running, don't make it look like a jump so much (or make the jumps smaller)
@@ -308,4 +299,5 @@ def menu(death_count):
 # Implement collisions so things blow up when you touch them
 # Would be cool to have stuff flying toward me
 
-
+pygame.display.quit()
+pygame.quit()
